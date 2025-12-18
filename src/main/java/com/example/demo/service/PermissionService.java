632e1df4681535a5d6953ef87ec22362d91729f4
bala-amypo <bala@amypo.in>
2025-12-18 15:50:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/PermissionService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Permission;
+
+@Service
+public class PermissionService {
+
+    Map<Long, Permission> mp = new HashMap<>();
+
+    public Permission createPermission(Permission permission) {
+        mp.put(permission.getId(), permission);
+        return permission;
+    }
+
+    public List<Permission> getAllPermissions() {
+        return new ArrayList<>(mp.values());
+    }
+
+    public Permission getPermissionById(Long id) {
+        return mp.get(id);
+    }
+
+    public Permission updatePermission(Long id, Permission permission) {
+        mp.replace(id, permission);
+        return permission;
+    }
+
+    public Permission deactivatePermission(Long id) {
+        Permission permission = mp.get(id);
+        if (permission != null) {
+            permission.setActive(false);
+            mp.put(id, permission);
+        }
+        return permission;
+    }
+}
