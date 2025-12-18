1447b02dfa5fb0558c4feff130dd77216f110009
bala-amypo <bala@amypo.in>
2025-12-18 08:49:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,1 +1,68 @@
-1
+1package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    public UserAccount(String email) {
+        this.email = email;
+    }
+    private String fullName;
+    private Boolean active;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    public UserAccount(Long id, String email, String fullName, Boolean active, LocalDate createdAt,
+            LocalDate updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.fullName = fullName;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    } 
+}
