834dfc209652a15dc6ec284badde25ee525ceffc
bala-amypo <bala@amypo.in>
2025-12-18 09:08:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/RolePermission.java
+++ src/main/java/com/example/demo/entity/RolePermission.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
