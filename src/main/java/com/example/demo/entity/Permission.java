6459611c510dd5a83d09ddd477aafddf443544e9
bala-amypo <bala@amypo.in>
2025-12-19 04:25:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Permission.java
+++ src/main/java/com/example/demo/entity/Permission.java
@@ -1,52 +1,66 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "permission")
 public class Permission {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String permissionKey;
+
     private String description;
+
     private Boolean active;
-    
-    
-   
+
+    public Permission() {
+    }
+
     public Permission(Long id, String permissionKey, String description, Boolean active) {
         this.id = id;
         this.permissionKey = permissionKey;
         this.description = description;
         this.active = active;
     }
-    public Permission() {
-    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getPermissionKey() {
         return permissionKey;
     }
+
     public void setPermissionKey(String permissionKey) {
         this.permissionKey = permissionKey;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public Boolean getActive() {
         return active;
     }
+
     public void setActive(Boolean active) {
         this.active = active;
     }
-
 }
