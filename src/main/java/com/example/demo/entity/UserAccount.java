b91dc971e934f3546abd8a4b253bed6c12d20bcd
bala-amypo <bala@amypo.in>
2025-12-19 04:37:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,70 +1,62 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
-
-import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user_account")
 public class UserAccount {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
-    private String email;
-    private String fullName;
+
+    private String username;
+    private String password;
     private Boolean active;
-    private LocalDate createdAt;
-    private LocalDate updatedAt;
-    
+
     public UserAccount() {
     }
-    public UserAccount(Long id, String email, String fullName, Boolean active, LocalDate createdAt,
-            LocalDate updatedAt) {
+
+    public UserAccount(Long id, String username, String password, Boolean active) {
         this.id = id;
-        this.email = email;
-        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
         this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public String getEmail() {
-        return email;
+
+    public String getUsername() {
+        return username;
     }
-    public void setEmail(String email) {
-        this.email = email;
+
+    public void setUsername(String username) {
+        this.username = username;
     }
-    public String getFullName() {
-        return fullName;
+
+    public String getPassword() {
+        return password;
     }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+  
+    public void setPassword(String password) {
+        this.password = password;
     }
+
     public Boolean getActive() {
         return active;
     }
+
     public void setActive(Boolean active) {
         this.active = active;
     }
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-    public LocalDate getUpdatedAt() {
-        return updatedAt;
-    }
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    } 
 }
